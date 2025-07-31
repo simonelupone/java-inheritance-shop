0cd2906f67989463c40e24eec1bb7c6f5154a081
simone <simone.lupone@gmail.com>
2025-07-31 12:34:33 +0200

    refactor: class DiscountUtility minor changes

--- src/org/lessons/java/shop/DiscountUtility.java
+++ src/org/lessons/java/shop/DiscountUtility.java
@@ -4,27 +4,27 @@ import java.math.BigDecimal;
 
 public class DiscountUtility<T extends Prodotto> {
   public BigDecimal discount(T prodotto, boolean discountCard){
-    if(!discountCard){
-      return prodotto.getPrice();
+    if(discountCard){
+      BigDecimal price = prodotto.getPrice();
+      BigDecimal discountedPrice = price.multiply(BigDecimal.valueOf(0.02));
+
+        if(prodotto instanceof Smartphone smartphone){
+          if(smartphone.getMemory() < 256){
+            discountedPrice = price.multiply(BigDecimal.valueOf(0.05));
+          }
+        } else if (prodotto instanceof Televisore televisore) {
+          if (televisore.getIsSmart()) {
+              discountedPrice = price.multiply(BigDecimal.valueOf(0.10));
+          }
+        } else if (prodotto instanceof Cuffie cuffie) {
+          if (cuffie.getIsWireless()) {
+              discountedPrice = price.multiply(BigDecimal.valueOf(0.07));
+          }
+      }
+      return price.subtract(discountedPrice);
     }
 
-    BigDecimal discountedPrice = new BigDecimal(0);
-    BigDecimal price = prodotto.getPrice();
-
-    if(prodotto instanceof Smartphone smartphone){
-      if(smartphone.getMemory() < 256){
-        discountedPrice = price.multiply(BigDecimal.valueOf(0.05));
-      }
-    } else if (prodotto instanceof Televisore televisore) {
-      if (televisore.getIsSmart()) {
-          discountedPrice = price.multiply(BigDecimal.valueOf(0.10));
-      }
-    } else if (prodotto instanceof Cuffie cuffie) {
-      if (cuffie.getIsWireless()) {
-          discountedPrice = price.multiply(BigDecimal.valueOf(0.07));
-      }
-  }
-  return price.subtract(discountedPrice);
+  return prodotto.getPrice();
   }
 }
 
